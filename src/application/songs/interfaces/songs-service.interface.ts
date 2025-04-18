40e66b4f560727a7e77f1b570c5b5dc8766f3a2a
Flávio Cândido <62729183+FlavioCandidoSilva@users.noreply.github.com>
2025-04-18 17:32:06 -0300

    to correct architecture

--- src/application/songs/interfaces/songs-service.interface.ts
+++ src/application/songs/interfaces/songs-service.interface.ts
@@ -1,6 +1,5 @@
 import { Song } from 'src/domain/songs/entities/song';
-import { CreateSongDto } from '../../data-transfer/songs/requests/create-song.dto';
 
 export interface ISongsService {
-  create(createSongDto: CreateSongDto): Promise<Song>;
+  // create(createSongDto: CreateSongDto): Promise<Song>;
 } 
--- src/infrastructure/mysql/songs/repositories/songs.repository.ts
+++ src/infrastructure/mysql/songs/repositories/songs.repository.ts
@@ -1,1 +1,12 @@
 
+import { FilterQuery } from '@mikro-orm/core';
+import { BaseRepository } from '../../shared/base.repository';
+import { Song } from 'src/domain/songs/entities/song';
+import { ISongsRepository } from 'src/domain/songs/repositories/songs.repository.interface';
+
+export class SongsRepository extends BaseRepository<Song> implements ISongsRepository {
+
+    constructor() {
+        super(Song);
+    }
+}
