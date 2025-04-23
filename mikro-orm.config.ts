ff4dcf094bf40568f4eb92022805a2d5e40bf8fc
Flávio Cândido <62729183+FlavioCandidoSilva@users.noreply.github.com>
2025-04-22 23:56:57 -0300

    Adjust docker file and Env
    Adjust docker file and Env

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig, MySqlDriver } from '@mikro-orm/mysql'; 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import { defineConfig, MySqlDriver } from '@mikro-orm/mysql';
 import { SongEntity } from 'src/infrastructure/database/songs/entities/song';
 
 export default defineConfig({
   driver: MySqlDriver,
-  host: process.env.DB_HOST || 'localhost',
-  port: +(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  dbName: process.env.DB_NAME || 'spoticraft',
+  host: process.env.DEV_DB_HOST || 'localhost',
+  port: +(process.env.DEV_DB_PORT || 3306),
+  user: process.env.DEV_DB_USERNAME || 'root',
+  password: process.env.DEV_DB_PASSWORD || '',
+  dbName: process.env.DEV_DB_NAME || 'spoticraft',
   entities: [SongEntity],
   entitiesTs: ['src/infrastructure/database/**/*.ts'],
   migrations: {
