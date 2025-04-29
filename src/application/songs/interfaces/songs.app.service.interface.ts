0c3522797c1126f361d1878dbf45e2e7c1baff53
Flávio Cândido <62729183+FlavioCandidoSilva@users.noreply.github.com>
2025-04-29 00:16:48 -0300

    create-upload-songs

--- src/application/songs/interfaces/songs.app.service.interface.ts
+++ src/application/songs/interfaces/songs.app.service.interface.ts
@@ -1,5 +1,6 @@
 import { CreateSongDto } from 'src/data-transfer/songs/requests/create-song.dto';
 import { UpdateSongDto } from 'src/data-transfer/songs/requests/update-song.dto';
+import { UploadSongDto } from 'src/data-transfer/songs/requests/upload.dto';
 import { Song } from 'src/domain/songs/entities/song';
 
 export abstract class ISongsAppService {
@@ -8,4 +9,5 @@ export abstract class ISongsAppService {
   abstract findOne(id: number): Promise<Song>;
   abstract update(id: number, updateSongDto: UpdateSongDto): Promise<void>;
   abstract remove(id: number): Promise<void>;
+  abstract uploadSong(file: Express.Multer.File, uploadSongDto: UploadSongDto): Promise<void>;
 } 
--- src/presentation/controllers/songs.controller.ts
+++ src/presentation/controllers/songs.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { ISongsAppService } from 'src/application/songs/interfaces/songs.app.service.interface';
 import { CreateSongDto } from 'src/data-transfer/songs/requests/create-song.dto';
 import { UpdateSongDto } from 'src/data-transfer/songs/requests/update-song.dto';
 import { Song } from 'src/domain/songs/entities/song';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { UploadSongDto } from 'src/data-transfer/songs/requests/upload.dto';
 
 @Controller('songs')
 export class SongsController {
@@ -33,5 +37,23 @@ export class SongsController {
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<void> {
     return this.songsAppService.remove(+id);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads/songs',
+      filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const ext = extname(file.originalname);
+        cb(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+      },
+    }),
+  }))
+  async uploadSong(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() uploadSongDto: UploadSongDto
+  ) {
+    return this.songsAppService.uploadSong(file, uploadSongDto);
   }
 } 
